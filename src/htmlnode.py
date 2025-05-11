17b3444e6c8d2893731e4194b79fb0979e9102df
Alan <144842474+SeccreDev@users.noreply.github.com>
2025-05-10 22:01:38 -0400

    add: LeafNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,7 +13,7 @@ class HTMLNode():
             return ""
         html = ""
         for key, value in self.props.items():
-            html += f"{key}=" + f'"{value}" '
+            html += f" {key}=" + f'"{value}"'
         return html
 
     def __eq__(self, other):
@@ -23,3 +23,18 @@ class HTMLNode():
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag == None:
+            return self.value
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/main.py
+++ src/main.py
@@ -6,5 +6,7 @@ def main():
     print(text_node)
     html_node = HTMLNode("h", "This is a heading", None, {"href": "https://www.google.com", "target": "_blank",})
     print(html_node)
+    leaf_node = LeafNode("h", "This is a heading", {"href": "https://www.google.com", "target": "_blank",})
+    print(leaf_node.to_html())
 
 main()
