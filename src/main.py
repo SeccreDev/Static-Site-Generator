57a24ddd1c1df2e50e56e15ae88d2595218060b3
Alan <144842474+SeccreDev@users.noreply.github.com>
2025-05-21 17:34:48 -0400

    feat: use generate_page function to create content inside the public directory

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from copy_directory import copy_source_to_directory
+from page_generation import generate_page
 
 def main():
-    source_directory = "./static"
-    destination_directory = "./public"
-    copy_source_to_directory(source_directory, destination_directory)
+    static_directory = "./static"
+    public_directory = "./public"
+    markdown_path = "./content/index.md"
+    template_path = "./template.html"
+
+    copy_source_to_directory(static_directory, public_directory)
+    generate_page(markdown_path, template_path, public_directory)
 
 if __name__ == "__main__":
     main()
